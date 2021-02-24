72c5b38e4deb4ef402dd4cad01a5234900d8a423
AhmedRamadanNU <ahmedramadan30188@gmail.com>
2021-02-24 03:12:40 +0200

    MCNP5 libraries benchmark

---
+++ case 6/batch/run 6.bat
@@ -0,0 +1,4 @@
+mcnp5 i=650c.txt o=o650c
+mcnp5 i=650d.txt o=o650d
+mcnp5 i=660c.txt o=o660c
+mcnp5 i=661c.txt o=o661c
---
+++ case 7/batch/run 7.bat
@@ -0,0 +1,4 @@
+mcnp5 i=750c.txt o=o750c
+mcnp5 i=750d.txt o=o750d
+mcnp5 i=760c.txt o=o760c
+mcnp5 i=761c.txt o=o761c
